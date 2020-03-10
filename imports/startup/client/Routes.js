52bd395aa6f26a86732dd872cbaf68d4b915ab09
Hermey Marc P. Villegas <hmpvillegas1998@gmail.com>
2020-03-10 14:43:01 +0800

    Renamed Dashboard file Fixed errors when switching dashboard component

--- imports/startup/client/Routes.js
+++ imports/startup/client/Routes.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Dashboard from '../../ui/components/dashboard/views/HelloWorld.js';
+import Dashboard from '../../ui/components/dashboard/views/Dashboard';
 import Application from '../../ui/components/dashboard/views/Application.js';
 
 // Middleware
@@ -12,10 +12,8 @@ export default class Routes extends Component {
     return (
       <Router>
         <Switch>
-        <ProtectedRoute path="/application" name="application" component={Application} />
-        <ProtectedRoute path="/" name="dashboard" component={Dashboard} />
-          
-          
+          <ProtectedRoute path="/application" name="application" component={Application} />
+          <ProtectedRoute path="/" name="dashboard" component={Dashboard} />
         </Switch>
       </Router>
     );
