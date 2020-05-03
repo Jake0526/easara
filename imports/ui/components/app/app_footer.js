c9939839f3b23ec3deb48baf3e60fd3337012f09
Jerico Talictic <jetalictic@gmail.com>
2020-05-04 03:18:22 +0800

    Updated version

--- imports/ui/components/app/app_footer.js
+++ imports/ui/components/app/app_footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AppFooter = () => (
   <footer className="main-footer">
   <div className="pull-right hidden-xs">
-      <b>Version</b> 0.0.1
+      <b>Version</b> 0.9.2
       </div>
   <strong>Employment Auto-Scheduler for Applicants for Revolving and Augmentations (EASARA) © 2020 </strong> All rights
   reserved.
